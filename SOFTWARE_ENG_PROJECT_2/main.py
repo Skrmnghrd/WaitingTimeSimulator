f1f43e84feb9d95a7864ae64ec12de03e62893a7
Skrmnghrd <paulogrian@gmail.com>
2019-11-21 13:42:12 -0600

    Uploaded
    Testing the repo for chronological purposes

--- SOFTWARE_ENG_PROJECT_2/main.py
+++ SOFTWARE_ENG_PROJECT_2/main.py
@@ -3,9 +3,14 @@ import sys
 #custom import paths
 sys.path.insert(0, "db_functs")
 sys.path.insert(0, "simulator")
-import __add__customer__
+import __add__customer__ #says and error but thigns run as fine
 
+#pass
 
+"""
+TEOIHKJHDKLJHA
+kawwakjdakhwd the quick brown fox jumped ove the lazy dog
+"""
 
 
 print('hello')
